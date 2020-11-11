4bf92d1051850d31eda4e2a490818de289351075
EleriasQueflunn <70859590+EleriasQueflunn@users.noreply.github.com>
2020-11-11 22:03:23 +0100

    Update color.py
    Configure color.py to be able to change the language in the program.

--- modules/base/console/color.py
+++ modules/base/console/color.py
@@ -4,47 +4,47 @@
 '''Module including color fonctions'''
 
 color__auth = 'lasercata'
-color__last_update = '03.11.2020'
-color__version = '2.2.1'
+color__last_update = '11.11.2020'
+color__version = '2.3'
 
 
 ##-import
 import ctypes
 import platform
 
-#from modules.base.base_functions import inp_lst
+from Languages.lang import translate as tr
 
 if platform.system() == 'Windows':
     handle = ctypes.windll.kernel32.GetStdHandle(-11)
 
     col = lambda x : ctypes.windll.kernel32.SetConsoleTextAttribute(handle, x)
 
-    dct_col = {'invisible' : 0, 'orange' : 1, 'blue' : 9, 'green' : 10, 'light_blue' : 11,
-    'red' : 12, 'pink' : 13, 'yellow' : 14, 'white' : 15}
+    dct_col = {tr('invisible'): 0, tr('orange'): 1, tr('blue'): 9, tr('green'): 10, tr('light_blue'): 11,
+    tr('red'): 12, tr('pink'): 13, tr('yellow'): 14, tr('white'): 15}
 
 
 elif platform.system() == 'Linux':
     col = lambda x : print('\033[' + str(x) + 'm', end='')
 
-    dct_col = {'none' : 0, 'bold' : 1, 'lite' : 2, 'italics' : 3, 'underline' : 4,
-    'flaches' : 5, 'norm' : 6, 'reverse' : 7, 'invisible' : 8, 'cross' : 9,
+    dct_col = {tr('none'): 0, tr('bold'): 1, tr('lite'): 2, tr('italics'): 3, tr('underline'): 4,
+    tr('flaches'): 5, tr('norm'): 6, tr('reverse'): 7, tr('invisible'): 8, tr('cross'): 9,
 
-    'black' : 30, 'red' : 31, 'green' : 32, 'orange' : 33, 'light_blue' : 34,
-    'pink' : 35, 'blue' : 35, 'white' : 37}
+    tr('black'): 30, tr('red'): 31, tr('green'): 32, tr('orange'): 33, tr('light_blue'): 34,
+    tr('pink'): 35, tr('blue'): 35, tr('white'): 37}
 
-c_prog = 'light_blue'
-c_input = 'green'
-c_error = 'red'
-c_wrdlt = 'pink'
-c_admin = 'green'
-c_succes = 'green'
-c_ascii = 'orange'
+c_prog = tr('light_blue')
+c_input = tr('green')
+c_error = tr('red')
+c_wrdlt = tr('pink')
+c_admin = tr('green')
+c_succes = tr('green')
+c_ascii = tr('orange')
 
 if platform.system() == 'Windows':
-    c_output = 'yellow'
+    c_output = tr('yellow')
 
 elif platform.system() == 'Linux':
-    c_output = 'blue'
+    c_output = tr('blue')
 
 
 ##-color
@@ -60,7 +60,7 @@ def color(choice_color):
         col(dct_col[choice_color])
 
     except KeyError:
-        print('The input is not a color !!!')
+        print(tr('The input is not a color') + ' !!!')
         return True
 
     else:
@@ -112,11 +112,11 @@ def cl_out_2(color_1, prompt, color_2, prompt_2, color_3=c_prog): #color_output_
 def c_use(c_use, c_old):
 
     while True:
-        c_in = cl_inp('Enter new ' + c_use + ' color (0 to keep the color) :')
+        c_in = cl_inp(tr('Enter new color of') + " " + c_use + " " + tr('(0 to keep the color)') + ' :')
         if c_in in ('0', ''):
             return c_old
         elif color(c_in):
-            cl_out(c_error, 'The color was NOT founded !')
+            cl_out(c_error, tr('The color was NOT founded') + ' !')
         else:
             return str(c_in)
 
@@ -130,41 +130,41 @@ def c_color():
     global c_ascii
 
     c_lst = [c_prog, c_input, c_output, c_error, c_wrdlt, c_succes, c_ascii]
-    c_lst_prnt = ('program', 'input', 'output', 'error', 'wordlist', 'succes', 'ascii art')
+    c_lst_prnt = (tr('program'), tr('input'), tr('output'), tr('error'), tr('wordlist'), tr('succes'), tr('ascii art'))
 
-    print('\nCurrent colors :\n')
+    print('\n' + tr('Current colors') + ' :\n')
     color(c_prog)
-    print('    Program color : ', c_prog)
+    print('    ' + tr('Program color') + ' :', c_prog)
     color(c_input)
-    print('    Input color : ', c_input)
+    print('    ' + tr('Input color') + ' :', c_input)
     color(c_output)
-    print('    Output color : ', c_output)
+    print('    ' + tr('Output color') + ' :', c_output)
     color(c_error)
-    print('    Error color :', c_error)
+    print('    ' + tr('Error color') + ' :', c_error)
     color(c_wrdlt)
-    print('    Wordlists color : ', c_wrdlt)
+    print('    ' + tr('Wordlists color') + ' :', c_wrdlt)
     color(c_succes)
-    print('    Succes color : ', c_succes)
+    print('    ' + tr('Succes color') + ' :', c_succes)
     color(c_ascii)
-    print('    Ascii art color : ', c_ascii)
+    print('    ' + tr('Ascii art color') + ' :', c_ascii)
     color(c_prog)
 
 
-    c = inp_lst('Change colors ? (y/n) :', ('y', 'n'))
+    c = inp_lst(tr('Change colors ?') + " " + tr('(y/n)') + ' :', (tr('y'), tr('n')))
 
-    if c == 'y':
-        ch = inp_lst('Restore default program colors (1) or change colors one by one (2) ? :', ('1', '2'))
+    if c == tr('y'):
+        ch = inp_lst(tr('Restore default program colors (1) or change colors one by one (2) ?') + ' :', ('1', '2'))
 
         if ch == '1':
-            c_prog = 'light_blue'
-            c_input = 'green'
-            c_output = 'yellow'
-            c_error = 'red'
-            c_wrdlt = 'pink'
-            c_succes = 'green'
-            c_ascii = 'orange'
+            c_prog = tr('light_blue')
+            c_input = tr('green')
+            c_output = tr('yellow')
+            c_error = tr('red')
+            c_wrdlt = tr('pink')
+            c_succes = tr('green')
+            c_ascii = tr('orange')
 
-            cl_out(c_succes, 'Done !\nBack menu ...')
+            cl_out(c_succes, tr('Done') + ' !\n' + tr('Back menu') + ' ...')
 
         else:
             for k in range(len(c_lst)):
@@ -188,6 +188,6 @@ def inp_lst(prompt, lst):
     c = ''
     c = cl_inp(prompt)
     while c not in lst:
-        cl_out(c_error, '\nWhat you entered is NOT in list !')
+        cl_out(c_error, '\n' + tr('What you entered is NOT in list') + ' !')
         c = cl_inp(prompt)
     return c
