ee9683a698fb074e7c7c2d4548189394e4577c81
EleriasQueflunn <70859590+EleriasQueflunn@users.noreply.github.com>
2021-02-03 23:30:20 +0100

    Update arithmetic.py
    Correct bug (negative gcd) and add a function solving the equation ax + by = c in Z.

--- modules/base/arithmetic.py
+++ modules/base/arithmetic.py
@@ -4,32 +4,32 @@
 """Program including useful arithmetic functions."""
 
 arithmetic__auth = 'Elerias'
-arithmetic__last_update = '23.09.2020'
-arithmetic__version = '1.0'
+arithmetic__last_update = '03.02.2020'
+arithmetic__version = '1.1'
 
 
 ##-functions
 
-def extended_gcd(a: int, b: int) -> (int, (int, int)):
+def ext_euclid(a: int, b: int) -> (int, (int, int)):
     """
     Extended Euclidean algorithm.
     Return great common divisor (gcd) and Bezout coefficients (u, v) for (a, b).
     u*a + v*b = gcd(a, b)
     Return (gcd, (u, v)).
-    Source of algorithm : https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm.
     """
-    
-    (old_r, r) = (a, b)
+
     (old_u, u) = (1, 0)
     (old_v, v) = (0, 1)
 
-    while r != 0:
-        q = old_r // r
-        (old_r, r) = (r, old_r - q*r)
+    while b != 0:
+        q = a // b
+        (a, b) = (b, a - q*b)
         (old_u, u) = (u, old_u - q*u)
         (old_v, v) = (v, old_v - q*v)
 
-    return old_r, (old_u, old_v)
+    if a<0:
+        return -a, (-old_u, -old_v)
+    return a, (old_u, old_v)
 
 
 def bezout(a: int, b: int) -> (int, int):
@@ -37,17 +37,16 @@ def bezout(a: int, b: int) -> (int, int):
     Return Bézout coefficients (u, v) for (a, b).
     u*a + v*b = gcd(a, b)
     """
-    
-    return extended_gcd(a, b)[1]
+
+    return ext_euclid(a, b)[1]
 
 
 def mult_inverse(a: int, n: int) -> int:
     """
     Return multiplicative inverse u of a modulo n.
     u*a = 1 modulo n
-    Source of algorithm : https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm.
     """
-    
+
     (old_r, r) = (a, n)
     (old_u, u) = (1, 0)
 
@@ -63,3 +62,27 @@ def mult_inverse(a: int, n: int) -> int:
         return old_u + n
     else:
         return old_u
+
+def solve_bezout_equation(a: int, b: int, c: int):
+    """
+    Solve the equation ax + by = c.
+    If there is no solution, return (False,), else return (True, ((m, n), (p, q))) with (mk + n, pk + q) the form of a solution.
+    """
+
+    (d,(u,v)) = ext_euclid(a,b)
+
+    if c % d != 0:
+        return (False,)
+
+    (a, b, c) = (a // d, b // d, c // d)
+
+    # au + bv = c
+    # ax + by = au + bv
+    # a(x-u) = -b(y-v)
+    # gcd(a,-b) = 1, we use the lemma of Gauss : y-v = ak, y = ak + v
+    # ax + b(ak+v) = au + bv
+    # ax + bak + bv = au + bv
+    # ax = au - kab
+    # x = -bk + u
+
+    return (True, ((-b, u), (a, v)))
